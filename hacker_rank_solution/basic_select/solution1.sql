20354c70e03b230c5bae2e54f0745eb3dce47982
simma-ezio <abirajasimman@gmail.com>
2025-08-22 18:07:09 +0530

    Create solution1.sql

---
+++ hacker_rank_solution/basic_select/solution1.sql
@@ -0,0 +1,7 @@
+SELECT * FROM CITY
+WHERE population>100000 AND countrycode="USA"
+
+-- Problem: Select countries in USA with population > 100000
+-- Link: https://www.hackerrank.com/challenges/[challenge-name]
+-- Concept: WHERE clause, basic filtering
+-- Date: 2025-08-22
