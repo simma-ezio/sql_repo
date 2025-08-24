1ab69782426e376eca48fc5e975baad884c165fb
simma-ezio <abirajasimman@gmail.com>
2025-08-25 01:33:40 +0530

    solution.sql
    solved one medium qn

--- hacker_rank_solution/basic_join/solution.sql
+++ hacker_rank_solution/basic_join/solution.sql
@@ -21,3 +21,27 @@ SELECT COUNTRY.Continent, FLOOR(AVG(CITY.Population)) FROM CITY
 INNER JOIN COUNTRY
     ON CITY.CountryCode=COUNTRY.Code
 GROUP BY COUNTRY.Continent
+
+--MEDIUM LEVEL QUESTION
+-- Problem: Two tables. Table 1: student_id, name, mark
+-- Table 2: Grade, min_mark, max_mark 
+-- Present a table with student name, grades, mark. Condition Only names of student grade above 8 must be present, remaining NULL
+-- Order should be grade descending, if same grade for grade > 8 order by name alphabetical, for <8 order by marks
+-- CONCEPT: Using CASE (If else) JOIN two tables without a foreign key
+ 
+SELECT
+    CASE
+        WHEN g.grade >= 8 THEN s.name
+        ELSE NULL
+    END AS name, g.grade, s.marks
+    FROM students s
+JOIN grades g 
+    ON s.marks BETWEEN g.min_mark AND g.max_mark
+ORDER BY
+    g.grade DESC,
+    CASE
+        WHEN g.grade >= 8 THEN s.name
+    END,
+    CASE
+        WHEN g.grade < 8 THEN s.marks
+    END
